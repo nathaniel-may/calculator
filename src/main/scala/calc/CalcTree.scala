a1a33bbb956d0273b6685ec9f076efd5adab59a5
Nathaniel May <nathanieldmay@gmail.com>
2019-06-15 15:47:40 -0400

    removed unnecessary stub

--- src/main/scala/calc/CalcTree.scala
+++ src/main/scala/calc/CalcTree.scala
@@ -1,13 +1,8 @@
 package calc
 
-import scala.util.Try
 import Calculator.Op
 
 sealed trait CalcTree
-object CalcTree {
-  def validate(tree: CalcTree): Try[CalcTree] = ???
-}
-
 object Empty extends CalcTree
 case class Literal(value: Double) extends CalcTree
 case class Operator(value: Op, inputs: (CalcTree, CalcTree)) extends CalcTree
