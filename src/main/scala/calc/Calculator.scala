6641f1e1cb36f673129b0a407db82b8b5be4fcf9
Nathaniel May <nathanieldmay@gmail.com>
2019-06-16 11:32:59 -0400

    moved OOO logic into evaluator

--- src/main/scala/calc/Calculator.scala
+++ src/main/scala/calc/Calculator.scala
@@ -3,11 +3,11 @@ package calc
 import scala.util.{Try, Success, Failure}
 import scala.annotation.tailrec
 import cats.implicits._
-import zipper._
 
 object Calculator {
-  sealed trait Order0Op
-  sealed trait Order1Op
+  sealed trait OOO
+  sealed trait Order0Op extends OOO
+  sealed trait Order1Op extends OOO
   sealed trait Op
   object Add  extends Op with Order1Op { override def toString: String = "+" }
   object Sub  extends Op with Order1Op { override def toString: String = "-" }
@@ -16,21 +16,6 @@ object Calculator {
   val ops = List(Add, Sub, Mult, Div)
 
   type EvalElem = Either[Double, Op]
-
-  implicit val unzippableOps: Unzip[CalcTree] = new Unzip[CalcTree] {
-    override def unzip(node: CalcTree): List[CalcTree] = node match {
-      case Empty               => Nil
-      case Literal(_)          => Nil
-      case Operator(_, (l, r)) => List(l, r)
-    }
-
-    override def zip(node: CalcTree, children: List[CalcTree]): CalcTree = (node, children) match {
-      case (Empty,           _)           => Empty
-      case (Literal(_),      _)           => Empty
-      case (Operator(op, _), l :: r :: _) => Operator(op, (l, r))
-      case _                              => Empty
-    }
-  }
 
   class CalcCompilationException(message: String) extends Exception(message)
   class CalcRuntimeException(message: String)     extends Exception(message)
@@ -46,185 +31,113 @@ object Calculator {
 
   def run(input: String): Try[Double] = for {
     elems  <- lex(input)
-    tree   <- parse(elems)
-    result <- eval(tree)
+    valid  <- parse(elems)
+    result <- eval(valid)
   } yield result
 
-  private[calc] def lex(input: String): Try[List[CalcTree]] =
+  private[calc] def lex(input: String): Try[List[EvalElem]] =
     input.split(' ').toList.map {
       case ""  => Failure(emptyInput)
-      case "+" => Success(Operator(Add,  (Empty, Empty)))
-      case "-" => Success(Operator(Sub,  (Empty, Empty)))
-      case "*" => Success(Operator(Mult, (Empty, Empty)))
-      case "/" => Success(Operator(Div, (Empty, Empty)))
+      case "+" => Success(Right(Add))
+      case "-" => Success(Right(Sub))
+      case "*" => Success(Right(Mult))
+      case "/" => Success(Right(Div))
       case num => Try(num.toDouble)
-        .fold(_ => Failure(invalidElem(num)), double => Success(Literal(double)))
+        .fold(_ => Failure(invalidElem(num)), double => Success(Left(double)))
     }
       .sequence
 
-  private[calc] def parse(input: List[CalcTree]): Try[CalcTree] = {
-    // TODO clunky
-    def validateStart(in: List[CalcTree]): Try[Unit] = in match {
-      case Operator(op, _) :: _ =>
-        Failure(missingLeftInput(op))
+  private[calc] def parse(input: List[EvalElem]): Try[List[EvalElem]] = {
 
-      case _ =>
-        Success(())
-    }
-
-    @tailrec
-    def validate(in: List[CalcTree], err: Try[Unit]): Try[Unit] = in match {
-      case Nil =>
-        err
-
-      case Empty :: _ =>
-        Failure(emptyInput)
-
-      case _ :: Empty :: _ =>
-        Failure(emptyInput)
-
-      case Operator(op0, _) :: Operator(op1, _) :: _ =>
-        Failure(invalidSeq(s"$op0 $op1"))
-
-      case Literal(l0) :: Literal(l1) :: _ =>
-        Failure(invalidSeq(s"$l0 $l1"))
-
-      case Operator(op, _) :: Nil =>
-        Failure(missingRightInput(op))
-
-      case Literal(_) :: tail =>
-        validate(tail, err)
-
-      case Operator(_, _) :: tail =>
-        validate(tail, err)
-    }
-
-    @tailrec
-    def pushdown(in: List[CalcTree], out: List[Operator]): Try[List[Operator]] = in match {
-      case Nil =>
-        Success(out.reverse)
-
-      case Literal(l) :: Operator(op: Order0Op, _) :: Literal(r) :: tail =>
-        pushdown(tail, Operator(op, (Literal(l), Literal(r))) :: out)
-
-      case Operator(op: Order0Op, _) :: Literal(r) :: tail =>
-        pushdown(tail, Operator(op, (Empty, Literal(r))) :: out)
-
-      case (op @ Operator(_: Order1Op, _)) :: tail =>
-        pushdown(tail, op :: out)
-
-      // I actually want to pattern match against op "not of type Order0Op"
-      case (l @ Literal(_)) :: Operator(op: Order1Op, _) :: tail =>
-        pushdown(tail, Operator(op, (l, Empty)) :: out)
-
-      // TODO this is cheating to get single literals to pass
-      case (l @ Literal(_)) :: tail =>
-        pushdown(tail, Operator(Mult, (l, Literal(1))) :: out)
-
-      case _ =>
-        Failure(boom)
-    }
-
-    def stitch(in: List[Operator]): CalcTree = {
-      @tailrec
-      def AddToRight(root: Zipper[CalcTree], child: CalcTree): Option[CalcTree] = {
-        Try(root.moveDownRight) match {
-          case Success(zNext) if (zNext.focus match {
-            case Empty => true;
-            case _ => false
-          }) => Some(zNext.update(_ => child).commit)
-
-          case Success(zNext) => AddToRight(zNext, child)
-
-          case _ => None
-        }
-      }
-
-      @tailrec
-      def AddToLeft(root: Zipper[CalcTree], child: CalcTree): Option[CalcTree] = {
-        Try(root.moveDownLeft) match {
-          case Success(zNext) if (zNext.focus match {
-            case Empty => true;
-            case _ => false
-          }) => Some(zNext.update(_ => child).commit)
-
-          case Success(zNext) => AddToLeft(zNext, child)
-
-          case _ => None
-        }
-      }
-
-      // TODO DELETE
-      def toString(tree: CalcTree): String = tree match {
-        case Empty => "EMPTY"
-        case Literal(l) => l.toString
-        case Operator(op, (l, r)) => s"${toString(l)} ${op.toString} ${toString(r)}"
-      }
-
-      def mergeIntoTree(tree: CalcTree, op: Operator): CalcTree =
-        AddToRight(Zipper(tree), op)
-          .fold(AddToLeft(Zipper(tree), op))(Some(_))
-          .fold(AddToRight(Zipper(op), tree))(Some(_))
-          .fold(AddToLeft(Zipper(op), tree))(Some(_)) get // TODO
-
-      def mergeIntoOp(tree: CalcTree, op: Operator): CalcTree =
-        (AddToRight(Zipper(op), tree)
-          .fold(AddToLeft(Zipper(op), tree))(Some(_))
-          .fold(AddToRight(Zipper(tree), op))(Some(_))
-          .fold(AddToLeft(Zipper(tree), op))(Some(_)) match {
-            case None =>
-              println()
-              println("TREE &&&&&&&&&&&&&&&&&&&&&&")
-              println(toString(tree))
-              println("OP &&&&&&&&&&&&&&&&&&&&&&")
-              println(toString(op))
-              None
-            case x => x
-        }) get // TODO
-
-      in.drop(1).foldLeft[CalcTree](in.head) { (tree, op) => (tree, op) match {
-        // when it OOO isn't in play, operators that come first get evaluated first
-        case (t @ Operator(_: Order1Op, _), o @ Operator(_: Order1Op, _)) =>
-          mergeIntoOp(t, o)
-
-        // when it OOO isn't in play, operators that come first get evaluated first
-        case (t @ Operator(_: Order0Op, _), o @ Operator(_: Order0Op, _)) =>
-          mergeIntoOp(t, o)
-
-        // respect OOO if possible (if not possible it's already been accounted for)
-        case (t @ Operator(_: Order1Op, _), o @ Operator(_: Order0Op, _)) =>
-          mergeIntoOp(t, o)
+    def validate(in: List[EvalElem]): Try[Unit] = {
+      def validateStart(in: List[EvalElem]): Try[Unit] = in match {
+        case Right(op) :: _ =>
+          Failure(missingLeftInput(op))
 
         case _ =>
-          mergeIntoTree(tree, op)
-      } }
+          Success(())
+      }
+
+      @tailrec
+      def go(in: List[EvalElem], err: Try[Unit]): Try[Unit] = in match {
+        case Nil =>
+          err
+
+        case Right(op0) :: Right(op1) :: _ =>
+          Failure(invalidSeq(s"$op0 $op1"))
+
+        case Left(l0) :: Left(l1) :: _ =>
+          Failure(invalidSeq(s"$l0 $l1"))
+
+        case Right(op) :: Nil =>
+          Failure(missingRightInput(op))
+
+        case Left(_) :: tail =>
+          go(tail, err)
+
+        case Right(_) :: tail =>
+          go(tail, err)
+      }
+
+      for {
+        _ <- validateStart(in)
+        _ <- go(in, Success(()))
+      } yield ()
     }
 
     for {
-      _   <- validateStart(input)
-      _   <- validate(input, Success(()))
-      ops <- pushdown(input, List())
-    } yield stitch(ops)
+      _ <- validate(input)
+    } yield input
   }
 
-  //TODO deal with results that are outside the double representation
-  private[calc] def eval(parsed: CalcTree): Try[Double] = parsed match {
-    case Empty        => Failure(new Exception("Internal error: attempted to evaluate empty parse tree"))
-    case Literal(num) => Success(num)
-    case Operator(op, (lTree, rTree)) => for {
-      l      <- eval(lTree)
-      r      <- eval(rTree)
-      math   =  op match {
-                  case Add  => l + r
-                  case Sub  => l - r
-                  case Mult => l * r
-                  case Div  => l / r
-                }
-      result <- if (math == Double.NegativeInfinity || math == Double.PositiveInfinity)
-                  Failure(divByZero)
-                else
-                  Success(math)
-    } yield result
-  }
+  private[calc] def eval(parsed: List[EvalElem]): Try[Double] = {
+    //TODO deal with results that are outside the double representation
+    def runOp(op: Op, args: (Double, Double)): Try[Double] = {
+      val math = (op, args) match {
+        case (Add,  (l, r)) => l + r
+        case (Sub,  (l, r)) => l - r
+        case (Mult, (l, r)) => l * r
+        case (Div,  (l, r)) => l / r
+      }
 
+      if (math == Double.NegativeInfinity || math == Double.PositiveInfinity)
+        Failure(divByZero)
+      else
+        Success(math)
+    }
+
+    // Throws runtime exceptions TODO right choice? probs not.
+    @tailrec
+    def evalPass0(ee: List[EvalElem], out: List[EvalElem]): List[EvalElem] = ee match {
+      case Left(_) :: Nil =>
+        out
+
+      case Left(l) :: Right(op: Order0Op) :: Left(r) :: tail =>
+        evalPass0((Left(runOp(op, (l, r)).get): EvalElem) :: tail, out)
+
+      case (l @ Left(_)) :: (op @ Right(_)) :: (r @ Left(_)) :: tail =>
+        evalPass0(r :: tail, l :: op :: out)
+
+      case _ =>
+        throw boom
+    }
+
+    // Throws runtime exceptions TODO right choice? probs not.
+    @tailrec
+    def evalPass1(ee: List[EvalElem], out: List[EvalElem]): Double = ee match {
+      case Left(result) :: Nil =>
+        result
+
+      case Left(l) :: Right(op) :: Left(r) :: tail =>
+        evalPass1((Left(runOp(op, (l, r)).get): EvalElem) :: tail, out)
+
+      case _ =>
+        throw boom
+    }
+
+    for {
+      pass0 <- Try(evalPass0(parsed, List.empty))
+      res   <- Try(evalPass1(pass0,  List.empty))
+    } yield res
+  }
 }
