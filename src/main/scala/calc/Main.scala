1904c21e064006d89bfc487c235a5b697c5894bb
Nathaniel May <nathanieldmay@gmail.com>
2019-06-16 13:06:36 -0400

    cleaned up error messages in the interface

--- src/main/scala/calc/Main.scala
+++ src/main/scala/calc/Main.scala
@@ -3,7 +3,7 @@ package calc
 import cats.effect.{IO, IOApp, ExitCode}
 import cats.syntax.all._
 
-object Main extends IOApp{
+object Main extends IOApp {
   val tooManyArgs = new Exception("Too many arguments. Expected a single string")
   val noArgs      = new Exception("Must pass a single string")
 
@@ -12,6 +12,6 @@ object Main extends IOApp{
     case Nil          => IO.raiseError(noArgs).as(ExitCode.Error)
     case input :: Nil => Calculator
       .run(input)
-      .fold(IO.raiseError(_).as(ExitCode.Success), output => IO(println(output)).as(ExitCode.Success))
+      .fold(e => IO(println(e.getMessage)).as(ExitCode.Success), output => IO(println(output)).as(ExitCode.Success))
   }
 }
