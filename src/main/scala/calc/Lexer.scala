62019a5f237d8ca071ec9d79b3e45dfbca0b08f6
Nathaniel May <nathanieldmay@gmail.com>
2019-06-23 19:29:43 -0400

    refactored run

--- src/main/scala/calc/Lexer.scala
+++ src/main/scala/calc/Lexer.scala
@@ -4,6 +4,7 @@ package calc
 import cats.syntax.all._
 import cats.instances.all._
 import cats.data.{NonEmptyList, StateT}
+
 import scala.util.matching.Regex
 import scala.util.Try
 
@@ -16,7 +17,12 @@ import calc.Instances._
 private[calc] object Lexer {
   type RegexLexer[A] = (Regex, String => Try[A])
 
-  def lex[A](rl: RegexLexer[A]): StateT[Try,String,A] = rl match {
+  val tokens: NonEmptyList[RegexLexer[Tok]] = NonEmptyList.of(
+    (TNum.regex, s => TNum.from(s).toTry(InvalidElementErr.from(s take 1))),
+    (TOp.regex,  s => TOp.from(s).toTry(InvalidElementErr.from(s take 1)))
+  )
+
+  def lex[A](rl: RegexLexer[A]): StateT[Try, String, A] = rl match {
     case (rx, build) =>
       StateT { s => for {
         matched <- rx.findPrefixMatchOf(s)
@@ -25,20 +31,9 @@ private[calc] object Lexer {
       } yield (s.drop(matched.end).trim, built) }
     }
 
-  val tokens: NonEmptyList[RegexLexer[Tok]] = NonEmptyList.of(
-    (TNum.regex, s => TNum.from(s).toTry(InvalidElementErr.from(s take 1))),
-    (TOp.regex,  s => TOp.from(s).toTry(InvalidElementErr.from(s take 1)))
-  )
+  def run(input: String): Try[List[Tok]] =
+    tokens.reduceMapK(lex)
+      .untilM[List](StateT.inspect(_.isEmpty))
+      .runA(input.trim)
 
-  def run(input: String): Try[List[Tok]] = {
-    def go: StateT[Try, String, List[Tok]] =
-      StateT.inspect[Try, String, Boolean](_.isEmpty) flatMap {
-        case true  => StateT.pure(List.empty)
-        case false => for {
-          tok  <- tokens.reduceMapK(lex)
-          toks <- go
-        } yield tok :: toks
-      }
-    go.runA(input.trim)
-  }
 }
