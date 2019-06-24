f58f9811e21986f58cc36f422cdf0f9a324a83f3
Nathaniel May <nathanieldmay@gmail.com>
2019-06-24 17:15:10 -0400

    parser back to being tail recursive

--- src/main/scala/calc/Parser.scala
+++ src/main/scala/calc/Parser.scala
@@ -5,14 +5,14 @@ import calc.Parse.{ParseTree, POp, PNum, LParen, RParen}
 import calc.Exceptions.{NothingToComputeErr, InvalidSequenceErr, MissingLeftInputErr, MissingRightInputErr, MismatchedParensErr}
 
 import scala.util.{Failure, Success, Try}
+import scala.annotation.tailrec
 
 // uses shunting-yard algorithm to build a parse tree. Could build BNF instead, but this is more generic.
 // https://en.wikipedia.org/wiki/Shunting-yard_algorithm
 private[calc] object Parser {
 
-  // TODO what does it think of "()"?
   def run(input: List[Tok]): Try[ParseTree] = {
-    //@tailrec
+    @tailrec
     def go(input: List[Tok], parseTree: List[ParseTree], shuntStack: List[Tok]): Try[ParseTree] = (input, parseTree, shuntStack) match {
       case (Nil, res ::  Nil, Nil) =>
         Success(res)
@@ -31,6 +31,9 @@ private[calc] object Parser {
 
       case (Nil, a :: b :: treeTail, TOp(op) :: shuntTail) =>
         go(Nil, POp(op, b, a) :: treeTail, shuntTail)
+
+      case (toks @ TParen(RParen) :: _, a :: b :: treeTail, TOp(op) :: shuntTail) =>
+        go(toks, POp(op, b, a) :: treeTail, shuntTail)
 
       case (TNum(num) :: tail, tree, stack) =>
         go(tail, PNum(num) :: tree, stack)
@@ -52,22 +55,17 @@ private[calc] object Parser {
       case ((p @ TParen(LParen)) :: tail, tree, stack) =>
         go(tail, tree, p :: stack)
 
-      case (TParen(RParen) :: tail, tree, stack) =>
-        def gogo(t: List[ParseTree], s: List[Tok]): Try[(List[ParseTree], List[Tok])] =
-          (t, s) match {
-            case (newT, TParen(LParen) :: sTail) =>
-              Success((newT, sTail))
+      case (TParen(RParen) :: tail, tree, TParen(LParen) :: stackTail) =>
+        go(tail, tree, stackTail)
 
-            case (a :: b :: tTail, TOp(op) :: sTail) =>
-              gogo(POp(op, b, a) :: tTail, sTail)
-
-            case _ =>
-              Failure(new MismatchedParensErr)
-          }
-        gogo(tree, stack) flatMap { case (newTree, newStack) => go(tail, newTree, newStack)}
-
-      case (_, _, TParen(_) :: _) =>
+      case (TParen(RParen) :: _, _, Nil) =>
         Failure(new MismatchedParensErr)
+
+      case (Nil, _, TParen(_) :: _) =>
+        Failure(new MismatchedParensErr)
+
+      case (toks, tree, TParen(LParen) :: sTail) =>
+        go(toks, tree, sTail)
 
       case (_, _ :: Nil, TOp(op) :: _) =>
         Failure(MissingRightInputErr.from(op))
