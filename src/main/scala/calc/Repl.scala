caa0111d15050187067e372e37e18a9da7663e39
Nathaniel May <nathanieldmay@gmail.com>
2019-06-25 13:36:16 -0400

    repl works, EOF cmd doesn't

--- src/main/scala/calc/Repl.scala
+++ src/main/scala/calc/Repl.scala
@@ -4,24 +4,25 @@ import calc.Main.{calculate, printIO, printResult, printlnIO, readLineIO}
 import cats.data.{NonEmptyList, StateT}
 import cats.effect.IO
 import cats.syntax.all._
+import cats.instances.all._
 
 object Repl {
-  type RunCMD = StateT[IO, String, Unit]
+  type RunCMD = StateT[Option, String, IO[Unit]]
   val header: String = ":::: This is the calc repl - type `exit` to quit ::::"
 
   val repl: IO[Unit] =
     printlnIO(header) *> go
 
-  val commands: NonEmptyList[(String => Boolean, String => IO[Unit])] = NonEmptyList(
-    (_ == null,        _   => printlnIO("EOF")), List(
-    (_.trim == "exit", _   => printIO("EXIT") >> IO(())),
-    (_.trim == ":q",   _   => printIO("QUIT") >> IO(())),
-    (_ => true,        cmd => printIO("CALC") >> printResult(calculate(cmd))._1 >> go))
+  val commands: NonEmptyList[(String => Boolean, String => IO[Unit])] = NonEmptyList.of(
+    (_ == null,        _   => printlnIO("")),
+    (_.trim == "exit", _   => IO(())),
+    (_.trim == ":q",   _   => IO(())),
+    (_ => true,        cmd => printResult(calculate(cmd))._1 >> go)
   )
 
   def runCmd(in: (String => Boolean, String => IO[Unit])): RunCMD = {
     val (check, run) = in
-    StateT.inspectF(s => if(check(s)) run(s) else printlnIO("BOOM"))
+    StateT.inspectF((s: String) => if (check(s)) Some (run(s)) else None)
   }
 
   def go: IO[Unit] = for {
@@ -31,6 +32,7 @@ object Repl {
     _   <- commands
             .reduceMapK(runCmd)
             .runA(cmd)
+            .getOrElse(IO(()))
   } yield ()
 
 }
