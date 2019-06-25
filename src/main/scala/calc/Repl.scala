0be4d9d346bceafee36c5f54e8d68919347ecde6
Nathaniel May <nathanieldmay@gmail.com>
2019-06-25 14:34:56 -0400

    eof exits shell properly

--- src/main/scala/calc/Repl.scala
+++ src/main/scala/calc/Repl.scala
@@ -13,16 +13,19 @@ object Repl {
   val repl: IO[Unit] =
     printlnIO(header) *> go
 
+  def notNullAnd(s: String)(f: String => Boolean): Boolean =
+    s != null && f(s)
+
   val commands: NonEmptyList[(String => Boolean, String => IO[Unit])] = NonEmptyList.of(
-    (_ == null,        _   => printlnIO("")),
-    (_.trim == "exit", _   => IO(())),
-    (_.trim == ":q",   _   => IO(())),
-    (_ => true,        cmd => printResult(calculate(cmd))._1 >> go)
+    (_ == null,                       _   => printlnIO("")),
+    (notNullAnd(_)(_.trim == "exit"), _   => IO(())),
+    (notNullAnd(_)(_.trim == ":q"),   _   => IO(())),
+    (notNullAnd(_)(_ => true),        cmd => printResult(calculate(cmd))._1 >> go)
   )
 
   def runCmd(in: (String => Boolean, String => IO[Unit])): RunCMD = {
     val (check, run) = in
-    StateT.inspectF((s: String) => if (check(s)) Some (run(s)) else None)
+    StateT.inspectF { s => if (check(s)) Some(run(s)) else None }
   }
 
   def go: IO[Unit] = for {
@@ -30,9 +33,9 @@ object Repl {
     _   <- printIO("calc> ")
     cmd <- readLineIO
     _   <- commands
-            .reduceMapK(runCmd)
-            .runA(cmd)
-            .getOrElse(IO(()))
+             .reduceMapK(runCmd)
+             .runA(cmd)
+             .getOrElse(IO(()))
   } yield ()
 
 }
